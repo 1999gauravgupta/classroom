5ad0543d2e985ddb46e7efab20eac79dc6050d34
Gaurav Gupta <gauravgupta11@hotmail.com>
2021-01-21 17:38:06 +0530

    fixing

--- plagiarism/settings.py
+++ plagiarism/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -142,3 +142,7 @@ SOCIAL_AUTH_GITHUB_KEY = os.environ['SOCIAL_AUTH_GITHUB_KEY']
 SOCIAL_AUTH_GITHUB_SECRET = os.environ['SOCIAL_AUTH_GITHUB_SECRET']
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
